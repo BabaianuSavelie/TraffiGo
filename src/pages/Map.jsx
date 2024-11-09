41145299d7ad1b85795cd6d056aa8b8a26c2e4fc
BabaianuSavelie <babaianu.savelie1@gmail.com>
2024-11-09 13:56:28 +0200

    Make test button invisible

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,14 +1,13 @@
-import map from '../assets/map_sign.png';
+import map from "../assets/map_sign.png";
 
 export default function Map() {
   return (
     <div>
-        <div
+      <div
         className="absolute object-cover bg-cover bg-center h-screen w-screen"
         style={{ backgroundImage: `url(${map})` }}
-        >
-        </div>
-        <button className="relative game-btn">Test Level</button>
+      ></div>
+      <button className="relative game-btn"></button>
     </div>
   );
 }
