2b9de01a460c852c2e5257cd2601fecb68452c9e
lbodlev888 <lbodlev888@gmail.com>
2024-11-09 14:22:59 +0200

    Added game router

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,13 +1,18 @@
 import map from "../assets/map_sign.png";
+import { useNavigate } from 'react-router-dom';
 
 export default function Map() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/game');
+  };
   return (
     <div>
       <div
         className="absolute object-cover bg-cover bg-center h-screen w-screen"
         style={{ backgroundImage: `url(${map})` }}
       ></div>
-      <button className="relative game-btn"></button>
+      <button className="relative game-btn" onClick={handleClick}></button>
     </div>
   );
 }
