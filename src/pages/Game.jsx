a48dc920dd92d0ef55dfd464b3179bb2b57585d3
BabaianuSavelie <babaianu.savelie1@gmail.com>
2024-11-09 13:06:21 +0200

    Make some changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
@@ -6,25 +6,42 @@ export default function Game() {
   const cityModel = useGLTF("./public/models/city/scene.gltf");
   const carModel = useGLTF("./public/models/car/scene.gltf");
 
+  const [carPosition, setCarPosition] = useState([0.9, 0.3, 12]);
+
   return (
     <div className="container">
       <Canvas
         camera={{
-          position: [0, 5, 20],
-          fov: 65,
+          position: [0, 4, 20],
+          fov: 85,
         }}
       >
-        <ambientLight intensity={1.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <OrbitControls />
         <Suspense fallback={<div>Loading...</div>}>
-          <primitive object={cityModel.scene} />
+          {/* <ambientLight intensity={1.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} /> */}
+          {/* Add Ambient Light */}
+          <ambientLight intensity={1.5} />
+
+          {/* Add SpotLight */}
+          <spotLight
+            position={[0, 5, 15]} // Adjust the position to focus on the car
+            angle={0.1}
+            intensity={5} // Increase intensity for more illumination
+            penumbra={1}
+            castShadow={true} // Optionally, enable shadows if needed
+          />
+
+          {/* Add a Directional Light to illuminate models */}
+          <directionalLight position={[5, 5, 5]} intensity={1} />
           <primitive
             object={carModel.scene}
-            position={[1, 0.3, 1]}
+            position={carPosition}
             scale={0.7}
+            rotation={[0, Math.PI, 0]}
           />
+          <primitive object={cityModel.scene} />
         </Suspense>
+        <OrbitControls />
       </Canvas>
     </div>
   );
